71f5cebc363d2fa930c5fd586b455aa501f9d46d
asdfbat <jonas.s.lunde@outlook.com>
2020-07-12 19:48:38 +0200

    Some bugfixes and updates to compression tests.

--- test_different_compressions.py
+++ test_different_compressions.py
@@ -3,13 +3,15 @@ import time
 import os
 from multiprocessing.pool import ThreadPool, Pool
 
-path = "/mn/stornext/d16/cmbco/comap/pathfinder/ovro/2019-05"
-filenames = ["comap-0005978-2019-05-18-011318.hd5",
-             "comap-0005979-2019-05-18-023832.hd5",
-             "comap-0005980-2019-05-18-043705.hd5",
-             "comap-0005981-2019-05-18-054046.hd5",]
+path = "/mn/stornext/d16/cmbco/comap/pathfinder/ovro/2020-06"
+target_path = "/mn/stornext/d16/cmbco/comap/jonas/compression"
+filenames = ["comap-0014481-2020-06-24-205832.hd5",
+             "comap-0014158-2020-06-11-132655.hd5",
+             "comap-0014148-2020-06-11-050935.hd5",
+             "comap-0014122-2020-06-10-083154.hd5",
+             "comap-0013865-2020-06-01-101139.hd5",]
 
-gzip_range = [1,2,3,4,5,6]
+gzip_range = [1,2,3,4,5,6,7]
 
 nr_threads = len(gzip_range)*len(filenames)
 print("Total of %d threads" % nr_threads)
@@ -21,12 +23,14 @@ for i in range(len(gzip_range)):
     for j in range(len(filenames)):
         distributed_gzip.append(gzip_range[i])
         distributed_filenames.append(filenames[j])
-        
+
+print("Runlist:", distributed_gzip, distributed_filenames)
+
 def compress(thread_idx):
     gzip_level = distributed_gzip[thread_idx]
     filename = distributed_filenames[thread_idx]
     raw_filepath = path + "/" + filename
-    comp_filepath = "comp_gzip%f_" + filename
+    comp_filepath = target_path + "/" + ("comp_gzip%d_" % gzip_level) + filename
     print("Thread %d running gzip=%d on file %s to file %s" % (thread_idx, gzip_level, raw_filepath, comp_filepath))
     
     t00 = time.time()
@@ -38,15 +42,14 @@ def compress(thread_idx):
     
     raw_size = os.path.getsize(raw_filepath)
     comp_size = os.path.getsize(comp_filepath)
-    print("Finished file %s oldsize=%.2fGB  newsize=%.2fGB  ratio=%.2f  time spent=%.1fm" % (raw_filepath, raw_size*1e-9, comp_size*1e-9, raw_size/comp_size, (t01-t00)/60.0))
+    print("Finished file %s with gzip %d oldsize=%.2fGB  newsize=%.2fGB  ratio=%.2f  time spent=%.1fm" % (comp_filepath, gzip_level, raw_size*1e-9, comp_size*1e-9, raw_size/comp_size, (t01-t00)/60.0))
 
 
 t0 = time.time()
 with Pool(nr_threads) as p:
-    p.map(compress, range(len(filenames)))
+    p.map(compress, range(nr_threads))
 t1 = time.time()
-print("Finished compression in %f minutes" % (t1-t0)/60.0)
-
+print("Finished compression in %f minutes" % ((t1-t0)/60.0))
 
 
 import h5py
@@ -57,10 +60,10 @@ for thread_idx in range(nr_threads):
     gzip_level = distributed_gzip[thread_idx]
     filename = distributed_filenames[thread_idx]
     raw_filepath = path + "/" + filename
-    comp_filepath = "comp_gzip%f_" + filename
+    comp_filepath = target_path + "/" + ("comp_gzip%d_" % gzip_level) + filename
     
     t0 = time.time()
-    data = h5py.File(raw_filepath, "r")
+    data = h5py.File(comp_filepath, "r")
     asdf = np.array(data["spectrometer/tod"])
     t1 = time.time()
     print("Finished file %s with GZIP=%d in %d seconds " % (filename, gzip_level, (t1-t0)) )
